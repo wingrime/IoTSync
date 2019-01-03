b0f672917c9fd106220585ebc93f7c97bafcce42
wingrime <wingrime@gmail.com>
2019-01-03 23:51:59 +0500

    Inital

---
+++ AuthMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace IoTSink
+{
+    public class AuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string authHeader = context.Request.Headers["Authorization"];
+            if (authHeader != null && authHeader.StartsWith("Basic"))
+            {
+                //Extract credentials
+                string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+                Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+                string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+                int seperatorIndex = usernamePassword.IndexOf(':');
+
+                var username = usernamePassword.Substring(0, seperatorIndex);
+                var password = usernamePassword.Substring(seperatorIndex + 1);
+
+                //TODO: Move to Config
+                if (username == _configuration["AuthLogin"] && password == _configuration["AuthPass"] )
+                {
+                    await _next.Invoke(context);
+                }
+                else
+                {
+                    context.Response.StatusCode = 401; //Unauthorized
+                    return;
+                }
+            }
+            else
+            {
+                // no authorization header
+                context.Response.StatusCode = 401; //Unauthorized
+                return;
+            }
+        }
+    }
+}
---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+namespace IoTSink.Controllers
+{
+    public class WeatherContract
+    {
+        public double Temp;
+        public double Co2;
+        public double Pressure;
+        public double Humidity;
+        public DateTime? Date;
+        public int DeviceId = 0;
+    }
+    
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly ILogger<WeatherController> _logger;
+        private readonly IConfiguration _configuration;
+
+        
+        private readonly IMongoCollection<WeatherContract> _weartherCollection;
+        
+        public WeatherController(ILogger<WeatherController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            //TODO: Move to IOC
+            var mongoClient = new MongoClient(MongoUrl.Create(_configuration["ConnectionString"]));
+            var db = mongoClient.GetDatabase("iot");
+            _weartherCollection = db.GetCollection<WeatherContract>("weather");
+        }
+        [HttpPost]
+        public void Post([FromBody] WeatherContract weather)
+        {
+            weather.Date = DateTime.UtcNow;
+            
+           _logger.LogInformation($"Co2={weather.Co2} Hum={weather.Humidity} Pressure={weather.Pressure} Temp={weather.Temp}");
+            
+            _weartherCollection.InsertOne(weather);
+
+        }
+
+       
+    }
+}
